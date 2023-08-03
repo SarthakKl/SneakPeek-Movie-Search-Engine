e864501cff4aedd45bf8a6001ee6a8552e0039da
Sarthak Kumar Lohani <sarthakk60@gmail.com>
2023-08-03 17:24:57 +0530

    fixed movie download feature

--- client/src/utils/DownloadPdf.js
+++ client/src/utils/DownloadPdf.js
@@ -3,6 +3,7 @@ import { jsPDF } from "jspdf";
 export const DownloadPdf = (data)=>{
   const doc = new jsPDF(); //this will create a pdf of A4 size
   // console.log("data coming to page : ", data);
+  
   let x = 10, y = 10;
   doc.text("Title",x,y);
   doc.text("Genre",x+100,y);
@@ -14,14 +15,19 @@ export const DownloadPdf = (data)=>{
       doc.addPage();
       y = 40;
     }
+    const genreNames = data[i].genre.map((g)=>g.name)
+    let genres = '';
+    for(let i = 0; i < 2; i++)
+      genres += genreNames[i] + ', ';
+    genres = genres.substring(0, genres.length - 2)
+
     console.log("Element at ", i ," is ",data[i]);
+
     doc.text(a+". "+data[i].title,x,y);
-    doc.text(data[i].genre[0].name+", "+data[i].genre[1].name,x+100,y);
+    doc.text(genres,x+100,y);
     doc.text(" "+data[i].rating,x+170,y);
     y+= 13;
     a++;
   }
-  
-  // console.log("something is wrong");
-  doc.save("MovieList.pdf"); //this command will download the file
+  doc.save("MovieList.pdf"); 
 }
